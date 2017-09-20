45c8fd86d82b9a4295c14ecf2891835d68860a38
zhangrui <zhangrui@ys.com>
2017-09-20 15:12:38 +0800

    change

--- src/pages/Approve.vue
+++ src/pages/Approve.vue
@@ -1,58 +1,44 @@
 <template>
-  <div class="flex-page">
+  <div>
     <top-nav :nav="nav">{{title}}</top-nav>
-    <list-view url="/example/api/studentCert.json">
-      <div v-for="(item, index) in listData" :key="index" style="height:100px;">{{item.name}}</div>
-    </list-view>
   </div>
 </template>
 
 <script>
+import { Cell } from 'vux'
 import TopNav from '@/components/TopNav'
-import ListView from '@/components/ListView'
 import { post } from '@/utils/ajax'
 
 export default {
   components: {
-<<<<<<< HEAD
-    TopNav, ListView
-=======
     Cell,
     TopNav
->>>>>>> ad7895981ac500195e168633b7408710362eb531
   },
   data () {
     return {
-      nav: [],
-      title: '',
+      title: ''
     }
   },
-  computed: {
-    listData () {
-      return this.$store.state.vux.listData
-    },
-  },
   created () {
-    this.title = this.$route.query.type == 1? '身份证申请': '居住证明申请'
-    post('/example/api/studentCert/groupByState.json').then(data => {
+    const type = 1
+    console.log(this.$store.state)
+    this.title = type == 1? '身份证申请': '居住证明申请'
+
+    post('/api/firePlan/info.json').then(data => {
       this.nav = [{
         link: 'fire_list?state=0',
-        num: data.init,
+        num: 13,
         text: '未检查',
         clsName: 'active'
       },{
         link: 'fire_list?state=1',
-        num: data.done,
+        num: 8,
         text: '已检查'
       },{
-        num: data.notify,
+        num: 19,
         text: '总次数'
       }]
     })
-  },
-  methods: {
-
   }
-
 }
 </script>
