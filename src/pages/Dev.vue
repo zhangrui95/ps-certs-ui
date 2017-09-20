2b8a91286262c3b36a88fd50b39ad670258c498a
mahailong <630078606@qq.com>
2017-09-20 08:56:46 +0800

    Dev

--- src/pages/Dev.vue
+++ src/pages/Dev.vue
@@ -21,7 +21,7 @@ export default {
     changeHref (e) {
       let { hash, menu } = e.target.dataset
       let userid = querystring.parse()['userid'] || ''
-      window.location.href = '#/'+hash+'?userid='+ userid +'&type'+menu
+      window.location.href = '#/'+hash+'?userid='+ userid +'&type='+menu
     }
   }
 }
