67485f585813916224bdcaaf1515b217a5e3c44d
zhangrui <zhangrui@ys.com>
2017-09-25 15:43:57 +0800

    change

--- src/components/UpLoading.vue
+++ src/components/UpLoading.vue
@@ -23,36 +23,45 @@
           sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
           sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
           success: function (res) {
-            let localId = res.localIds[0]
-            let localData = res.localData
-            let url = localData || localId
-            console.log('url' + url)
-            _this.upload(res, _this.count)
+            _this.getLocalImgData(res, _this.count)
+            _this.upload(res)
           },
           fail: function (err) {
             console.log('error')
           }
         })
       },
-      upload: function (res, count) {
-        let _localIds = res.localIds
+      getLocalImgData: function (res, count) {
+        let ioslocId = []
         let _this = this
+        for (let i = 0, len = res.localIds.length; i < len; i++) {
+          this.$wechat.getLocalImgData({
+            localId: res.localIds[i],
+            success: function (res) {
+              let localData = res.localData
+              let imgs = {}
+              imgs.src = localData.replace('jgp', 'jpeg')
+              if (count === 9) {
+                _this.ViewImages.push(imgs)
+              } else {
+                ioslocId.push(imgs)
+                _this.ViewImages = ioslocId
+              }
+              _this.$emit('addImages', _this.ViewImages)
+            },
+            fail: function (err) {
+            }
+          })
+        }
+      },
+      upload: function (res) {
+        let _localIds = res.localIds
         this.$wechat.uploadImage({
           localId: _localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
           isShowProgressTips: 1, // 默认为1，显示进度提示
           success: function (res) {
-            let Imgsrc = []
-            for (var i = 0, len = _localIds.length; i < len; i++) {
-              let imgs = {}
-              imgs.src = "'" + _localIds[i] + "'"
-              if (count === 9) {
-                _this.ViewImages.push(imgs)
-              } else {
-                Imgsrc.push(imgs)
-                _this.ViewImages = Imgsrc
-              }
-            }
-            _this.$emit('addImages', _this.ViewImages)
+            let serverId = res.serverId
+            console.log(serverId)
           },
           fail: function (err) {
           }
