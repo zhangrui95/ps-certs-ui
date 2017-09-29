655f296cc390d7785cc0a8b161023c1851d2a93e
wangxiaodong <wangxiaodong@ys.com>
2017-09-29 17:25:58 +0800

    new list view

---
+++ src/components/ListView2.vue
@@ -0,0 +1,106 @@
+<template>
+  <div ref="wrapper" class="list-box">
+    <div class="scroll-content">
+      <div class="list-wrap">
+        <!--<div class="approve-list" v-for="group in groups" :key="group.dateStr">-->
+          <!--<div class="group-title">-->
+            <!--{{group.dateStr}}-->
+          <!--</div>-->
+        <slot v-for="(item, index) in list" :item="item" :index="index"></slot>
+        <!--</div>-->
+      </div>
+      <div class="pullup-wrapper">
+        <div class="before-trigger" v-if="!isPullUpLoad">
+          <span>{{isNoMore? '暂时没有更多': '加载更多'}}</span>
+        </div>
+        <div class="after-trigger" v-else>
+          加载中<inline-loading></inline-loading>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+  import { InlineLoading } from 'vux'
+  import { post } from '@/utils/ajax'
+
+  export default {
+    components: {
+      BScroll, InlineLoading,
+    },
+    props: ['url', 'params', 'list', 'startY'],
+    data() {
+      return {
+        isPullUpLoad: false,
+        isNoMore: false,
+        scrollY: 0,
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this.initScroll()
+      },200)
+    },
+    methods: {
+      initScroll() { //初始化scroll
+        this.scroll = new BScroll(this.$refs.wrapper, { click: true, pullUpLoad: true })
+//        if (this.list.length > 0) { // 如果已有数据 滚动到指定位置 否则首次获取数据
+//          this.$nextTick(() => {
+//            this.scrollY = this.startY || 0
+//            this.scroll.scrollTo(0,this.scrollY,0)
+//          })
+//          if (this.list.length < 10) this.isNoMore = true
+//        } else {
+//          this.getListData()
+//        }
+//        this.bindScrollEvent()
+      },
+      getListData(params) {
+        this.isPullUpLoad = true
+//        post(this.url, { max: 10, offset: this.list.length, ... this.params, ...params }).then(data => {
+//          this.$emit('update', data) // 将获取的数据传递给父组件
+//          if (data.list.length < 10) this.isNoMore = true  // 如果list长度为0  显示“暂时没有更多”
+//          this.$nextTick(() => {
+//            this.scroll.refresh()
+//            this.scroll.finishPullUp()
+//            this.isPullUpLoad = false
+//          })
+//        })
+      },
+      refresh () {  // 刷新
+        this.$nextTick(() => {
+          this.scroll.scrollTo(0,0,0)
+          this.getListData({offset: 0})
+        })
+      },
+      bindScrollEvent() {  // 绑定滚动和上拉事件
+//        this.scroll.on('scrollEnd', ({x, y = 0}) => this.scrollY = y )
+//        this.scroll.on('pullingUp', () => {
+//          if (!this.isPullUpLoad && !this.isNoMore) this.getListData()
+//        })
+      },
+      getScrollY() {  // 获取滚动的高度
+        return this.scrollY
+      }
+    },
+  }
+</script>
+
+<style lang="less">
+  .list-box{
+    flex: 1;
+    overflow: hidden;
+    background: #fff;
+    box-sizing: border-box;
+  }
+  .pullup-wrapper{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 16px 0;
+  }
+
+</style>
--- src/pages/Done.vue
+++ src/pages/Done.vue
@@ -2,19 +2,13 @@
   <div class="flex-page">
     <div class="header-box">已完成({{count}})</div>
     <list-view url="api/studentCert.json" :list="listData" :params="params" :startY="scrollY" @update="update" ref="listView">
-      <div class="list-wrap">
-        <div class="approve-list" v-for="group in groups" :key="group.dateStr">
-          <div class="group-title">
-            {{group.dateStr}}
-          </div>
-          <div v-for="(item, index) in group.items" :key="item.id"
-            @click="linkTo(`/Detail?id=${item.id}`)" class="list-item">
-            <span class="item-index">{{index > 8? index + 1: '0' + (index + 1)}}.</span>
-            <span class="item-title">{{item.name}}</span>
-            <badge v-if="item.result == -1" text="已退回"/>
-          </div>
+      <template scope="props">
+        <div @click="linkTo(`/Detail?id=${props.item.id}`)" class="list-item">
+          <span class="item-index">{{props.index > 8? props.index + 1: '0' + (props.index + 1)}}.</span>
+          <span class="item-title">{{props.item.name}}</span>
+          <badge v-if="props.item.result == -1" text="已退回"/>
         </div>
-      </div>
+      </template>
     </list-view>
     <filtrate-pop ref="filtrate" @submit="submit"/>
   </div>
@@ -22,9 +16,11 @@
 
 <script>
 import { Badge, dateFormat } from 'vux'
-import ListView from '../components/ListView'
+import { createNamespacedHelpers } from 'vuex'
+import ListView from '../components/ListView2'
 import FiltratePop from '../components/FiltratePop'
-import service from '../api/studentCert'
+
+const { mapActions, mapState } = createNamespacedHelpers('studentCert')
 
 export default {
   components: {
@@ -34,55 +30,68 @@ export default {
     return {
       count: 0,
       scrollY: 0,
-      params: {},
-      listData: []
+      params: {}
     }
   },
   computed: {
+    ...mapState({
+      listData: state => state.list
+    }),
     groups () {
+      let map = this.listData.reduce((target, item) => {
+        const dateStr = dateFormat(item.createTime, 'YYYY年MM月DD日')
+        if(target.filter(v => v.dateStr === dateStr).length === 0){
+
+        }else{
+
+        }
+        return target
+      }, [])
+      console.log('map', map)
       return this.listData
         .map(item => dateFormat(item.createTime, 'YYYY年MM月DD日'))
-        .filter((dateStr, i, arr) =>  arr.indexOf(dateStr, i+1) == -1 )
+        .filter((dateStr, i, arr) => arr.indexOf(dateStr, i + 1) === -1)
         .map(dateStr => {
-          return { dateStr, items: this.listData.filter(item => item.dateStr == dateStr)}
+          return {dateStr, items: this.listData.filter(item => item.dateStr === dateStr)}
         })
     }
   },
-  watch : {
-    '$store.state.router' () {
-      this.scrollY = this.$store.state.router.scrollY
-      this.params = this.$store.state.router.params
-      this.listData = this.$store.state.router.listData
-    }
-  },
+//  watch : {
+//    '$store.state.router' () {
+//      this.scrollY = this.$store.state.router.scrollY
+//      this.params = this.$store.state.router.params
+//      this.listData = this.$store.state.router.listData
+//    }
+//  },
   created () {
     this.params = { state: 2 }
-    const data = service.list(this)
-    console.log('post2 data', data)
-//    this.count = data.count
+    this.list()
   },
   methods: {
+    ...mapActions({
+      list: 'list'
+    }),
     update (data) {
-      this.count = data.count
-      this.listData = [ ...this.listData, ...data.list.map(item => {
-        return {...item, dateStr:dateFormat(item.createTime, 'YYYY年MM月DD日')}
-      })]
+//      this.count = data.count
+//      this.listData = [ ...this.listData, ...data.list.map(item => {
+//        return {...item, dateStr:dateFormat(item.createTime, 'YYYY年MM月DD日')}
+//      })]
     },
     submit (val) {
-      this.params = {...this.params, name: val}
-      this.listData = []
-      this.$refs.listView.refresh()
+//      this.params = {...this.params, name: val}
+//      this.listData = []
+//      this.$refs.listView.refresh()
     },
     commitState () {
-      this.$store.commit('updateRouterState', {
-        params: this.params,
-        listData: this.listData,
-        scrollY: this.$refs.listView.getScrollY()
-      })
+//      this.$store.commit('updateRouterState', {
+//        params: this.params,
+//        listData: this.listData,
+//        scrollY: this.$refs.listView.getScrollY()
+//      })
     },
     linkTo (url) {
-      this.commitState()
-      this.$router.push(url)
+//      this.commitState()
+//      this.$router.push(url)
     }
   }
 }
