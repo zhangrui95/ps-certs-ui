6f41d52c0cde1e5c52381a6b45bbb0f0f9631d7a
wangxiaodong <wangxiaodong@ys.com>
2017-10-09 15:28:39 +0800

    list view split to load more / list / page

---
+++ src/components/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="list-wrap">
+    <slot v-for="(item, index) in list" :item="item" :index="index"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      list: {
+        type: Array,
+        'default': []
+      }
+    }
+  }
+</script>
