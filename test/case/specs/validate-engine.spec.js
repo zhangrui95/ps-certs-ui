9ca17d8d6a3182f326ae7c4fc43c614e7d6154df
zhangrui <zhangrui@ys.com>
2017-10-11 17:37:10 +0800

    change test

--- test/case/specs/validate-engine.spec.js
+++ test/case/specs/validate-engine.spec.js
@@ -1,17 +1,18 @@
 import chai from 'chai'
 import engine from '../../../src/plugins/validate-plugin/engine'
+import testList from './test-data'
 
 const expect = chai.expect
 
-describe('test engine', () => {
-  it('should card rule function', () => {
-    const ret = engine.validate({rule: 'card', value: '1'})
-    expect(ret)
-      .to.equal(false)
-  })
-  it('should card rule true', () => {
-    const ret = engine.validate({rule: 'card', value: '2'})
-    expect(ret)
-      .to.equal(true)
+describe('测试engine', () => {
+  testList.forEach(function (item) {
+    it('测试' + item.rule + '：' + item.value + ' validator 应该返回：' + item.ret, () => {
+      let fn = function (item) {
+        const ret = engine.validate({rule: item.rule, value: item.value})
+        expect(ret)
+          .to.equal(item.ret)
+      }
+      return fn
+    })
   })
 })
