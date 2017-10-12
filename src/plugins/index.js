210c48ecd29fd5efc2463f4d6560a2a72d720a8d
wangxiaodong <wangxiaodong@ys.com>
2017-10-12 10:35:09 +0800

    add mocha func unit test

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import { dateFormat, BusPlugin, DatetimePlugin, LocalePlugin, DevicePlugin, ToastPlugin, AlertPlugin, ConfirmPlugin, LoadingPlugin, WechatPlugin, AjaxPlugin, AppPlugin } from 'vux'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
-import MockWechatPlugin from '../mock/wx-mock'
+import MockWechatPlugin from './wechat-mock-plugin'
 import FastClick from 'fastclick'
 
 // plugins
---
+++ test/case/specs/test.spec.js
@@ -0,0 +1,11 @@
+import chai from 'chai'
+
+const expect = chai.expect
+
+describe('test', () => {
+  it('should test contents', () => {
+    const w = '1'
+    expect(w)
+      .to.equal('1')
+  })
+})
